bbdaf9083f4be5630d5321c2a0e74b834de4ee07
SeungDae321 <110653719+SeungDae321@users.noreply.github.com>
2023-01-19 16:14:43 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ import random
 def dice_num():
     return random.randrange(1,7)
 
-intro =     '''
+play_again = False
+
+def play_the_Game():
+    intro =     '''
   __________________
    # ROLL THE DICE!#
   ㅡㅡㅡㅡㅡㅡㅡㅡㅡ
@@ -11,52 +14,61 @@ intro =     '''
          
          '''      
 
-input(intro)
-
-user_score1 = dice_num()
-print(
-f'''
-#################################
-     Your dice choose:          
-            {user_score1}       
-    You have to ROLL AGAIN!    
-#################################
-        
-''')
-
-input(intro)
-
-user_score2 = dice_num()
-
-print(f'''
-#############################################################
-                 now your dice number is                    
-                     {user_score1} + {user_score2} = {user_score1 + user_score2} 
-#############################################################
- ''')
-
-if user_score1 == user_score2:
-    print(f'''You have the DOUBLE!{user_score1, user_score2}''')
     input(intro)
-    chance_score = dice_num()
-    print(
-    f'''
-    #################################################
 
-    Your totall score is {user_score1} + {user_score2} + {chance_score} = {user_score1 + user_score2 + chance_score}
+    user_score1 = dice_num()
+    print(
+        f'''
+        #################################
+            Your dice choose:          
+                {user_score1}       
+            You have to ROLL AGAIN!    
+        #################################
+        
+        ''')
+
+    input(intro)
+
+    user_score2 = dice_num()
+
+    print(f'''
+        #############################################################
+                         now your dice number is                    
+                             {user_score1} + {user_score2} = {user_score1 + user_score2} 
+        #############################################################
+        ''')
     
-    #################################################
-    '''
-    )
+    
 
-if user_score1 == user_score2 == chance_score:
-    print(
-        '''
-            ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
-           | ARE YOU KIDDING ME?|
-            ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+    if user_score1 == user_score2:
+        print(f'''You have the DOUBLE!{user_score1, user_score2}''')
+        input(intro)
+        chance_score = dice_num()
+        print(
+        f'''
+        #################################################
+
+        Your totall score is {user_score1} + {user_score2} + {chance_score} = {user_score1 + user_score2 + chance_score}
+    
+        #################################################
         '''
         )
+
+    if user_score1 == user_score2 == chance_score:
+        print(
+            '''
+                ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+                | ARE YOU KIDDING ME?|
+                ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+            '''
+             )
+    user_game = input('Do you want to play again? (Y/N)')
+    
+    if user_game == 'Y' or user_game == 'y':
+        play_the_Game()
+
+play_the_Game()
+
 
 
 
